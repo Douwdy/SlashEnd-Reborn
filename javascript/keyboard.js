ba677e526fdac496e418041847da772f06790cd8
Douwdy <mrslim65.gl@gmail.com>
2022-04-05 22:47:51 +0200

    Keyboard App Start

---
+++ javascript/keyboard.js
@@ -0,0 +1,17 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        onKeyDown: null,
+        onKeyUp: null
+    },
+
+    proterties: {
+        value: "",
+        capsLock: false,
+    }
+};
